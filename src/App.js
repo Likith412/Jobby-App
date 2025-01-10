8dc71352621db9ca5acb4cc2676387de9636596f
Likith Metikala <metikalalikithkumar.gtl@gmail.com>
2025-01-10 20:54:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import {Switch, Route, Redirect} from 'react-router-dom'
-import Home from './components/Home'
-import Jobs from './components/Jobs'
-import JobItemDetails from './components/JobItemDetails'
-import Login from './components/Login'
-import NotFound from './components/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./components/Home";
+import Jobs from "./components/Jobs";
+import JobItemDetails from "./components/JobItemDetails";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-import './App.css'
+import "./App.css";
 
 const App = () => (
   <>
@@ -19,6 +19,6 @@ const App = () => (
       <Redirect to="/not-found" />
     </Switch>
   </>
-)
+);
 
-export default App
+export default App;
